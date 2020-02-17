0e012feaca045c6551b7129f8495d1f3f03d7d5c
Jan Müller <janmueller3698@gmail.com>
2020-02-17 10:39:12 +0100

    Created AccountFragment

--- app/src/main/java/de/uniks/codliners/stock_simulator/MainActivity.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_account, R.id.navigation_notifications, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/account/AccountViewModel.kt
@@ -0,0 +1,5 @@
+package de.uniks.codliners.stock_simulator.ui.account
+
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel : ViewModel()
