1ed2b81cf23ed19ca5d764214ea0b28349d4f616
lucasheld <lucasheld@hotmail.de>
2020-02-17 16:33:31 +0100

    added settings to stockbrot

--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/BindingAdapters.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/BindingAdapters.kt
@@ -1,7 +1,9 @@
 package de.uniks.codliners.stock_simulator.ui
 
+import android.widget.Button
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import de.uniks.codliners.stock_simulator.R
 import de.uniks.codliners.stock_simulator.domain.Share
 import de.uniks.codliners.stock_simulator.ui.account.DepotShareRecyclerViewAdapter
 
@@ -9,4 +11,13 @@ import de.uniks.codliners.stock_simulator.ui.account.DepotShareRecyclerViewAdapt
 fun RecyclerView.bindDepotShares(shares: List<Share>?) {
     val adapter = adapter as DepotShareRecyclerViewAdapter
     adapter.submitList(shares)
+}
+
+@BindingAdapter("botEnabled")
+fun Button.bindBotEnabled(enabled: Boolean) {
+    text = if (enabled) {
+        context.getText(R.string.stockbrot_disable_bot)
+    } else {
+        context.getText(R.string.stockbrot_enable_bot)
+    }
 }
--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/stockbrot/StockbrotViewModel.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/stockbrot/StockbrotViewModel.kt
@@ -1,5 +1,28 @@
 package de.uniks.codliners.stock_simulator.ui.stockbrot
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class StockbrotViewModel : ViewModel()
+class StockbrotViewModel : ViewModel() {
+    private var _enabled = MutableLiveData<Boolean>(false)
+    var enabled: LiveData<Boolean> = _enabled
+
+    fun toggleBot() {
+        println("pressed button toggle bot")
+        when(enabled.value) {
+            true -> disableBot()
+            false -> enableBot()
+        }
+    }
+
+    private fun enableBot() {
+        println("enable bot")
+        _enabled.value = true
+    }
+
+    private fun disableBot() {
+        println("disable bot")
+        _enabled.value = false
+    }
+}
