a1cf073cc24c089a788f296a73197ab5ae30c3f0
lucasheld <lucasheld@hotmail.de>
2020-02-17 13:13:41 +0100

    display shares in accountView

--- app/src/main/java/de/uniks/codliners/stock_simulator/domain/DomainModels.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/domain/DomainModels.kt
@@ -1,3 +1,9 @@
 package de.uniks.codliners.stock_simulator.domain
 
-data class Share(val id: Long)
+data class Share(
+    val id: String,
+    val name: String,
+    val price: Double,
+    val gap: Double,
+    val gapPercent: Double
+)
