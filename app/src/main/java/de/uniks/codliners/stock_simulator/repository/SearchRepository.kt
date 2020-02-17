8cdf851983f19099db2b0c5f70e319a20973de4e
Juri <juri.lozowoj@freenet.de>
2020-02-17 11:11:33 +0100

    added refreshShares method

--- app/src/main/java/de/uniks/codliners/stock_simulator/repository/SearchRepository.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/repository/SearchRepository.kt
@@ -5,12 +5,21 @@ import androidx.lifecycle.Transformations
 import de.uniks.codliners.stock_simulator.database.StockDatabase
 import de.uniks.codliners.stock_simulator.database.asDomainModel
 import de.uniks.codliners.stock_simulator.domain.Share
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class SearchRepository(private val stockDatabase: StockDatabase) {
 
     val shares: LiveData<List<Share>> = Transformations.map(stockDatabase.shareDao.getShares()) {
         it.asDomainModel()
     }
-    
+
+    suspend fun refreshShares() {
+        withContext(Dispatchers.IO) {
+            // val shareList = Network.shareService.getShares().await()
+            // stockDatabase.shareDao.insertAll(*shareList.asDatabaseModel())
+        }
+    }
+
 }
 
