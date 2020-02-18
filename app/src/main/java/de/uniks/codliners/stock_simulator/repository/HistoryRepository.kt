9a4970e5d26d14e6f07a287201445aceb442ab3c
Juri <juri.lozowoj@freenet.de>
2020-02-18 11:31:14 +0100

    added historyRepository to historyViewModel

--- app/src/main/java/de/uniks/codliners/stock_simulator/repository/HistoryRepository.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/repository/HistoryRepository.kt
@@ -1,18 +1,29 @@
 package de.uniks.codliners.stock_simulator.repository
 
+import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import de.uniks.codliners.stock_simulator.database.StockAppDatabase
+import de.uniks.codliners.stock_simulator.database.getDatabase
 import de.uniks.codliners.stock_simulator.database.transactionsAsDomainModel
 import de.uniks.codliners.stock_simulator.domain.Transaction
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class HistoryRepository(private val stockAppDatabase: StockAppDatabase) {
+
+    constructor(application: Application): this(getDatabase(application))
 
     sealed class State {
         object Loading: State()
         object Done: State()
         class Error(val message: String)
     }
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State> = _state
 
     val transactions: LiveData<List<Transaction>> =
         Transformations.map(stockAppDatabase.transactionDao.getTransactions()) {
--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryViewModel.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryViewModel.kt
@@ -1,37 +1,41 @@
 package de.uniks.codliners.stock_simulator.ui.history
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import android.app.Application
+import androidx.lifecycle.*
 import de.uniks.codliners.stock_simulator.domain.Share
 import de.uniks.codliners.stock_simulator.domain.Transaction
 import de.uniks.codliners.stock_simulator.domain.TransactionType
 import de.uniks.codliners.stock_simulator.repository.HistoryRepository
+import de.uniks.codliners.stock_simulator.ui.quote.QuoteViewModel
 import kotlinx.coroutines.launch
 import java.lang.Exception
 
-class HistoryViewModel : ViewModel() {
+class HistoryViewModel(application: Application) : ViewModel() {
 
-//    private val historyRepository = HistoryRepository()
+    private val historyRepository = HistoryRepository(application)
+
+    // private val state = historyRepository.state
+    val transactions = historyRepository.transactions
+
 
     val shareOne = Share("AA", "american airline", 20.0, 0.2, 0.2, 0.1)
     val shareTwo = Share("SMA", "sma ", 19.0, 0.2, 0.2, 0.1)
 
     val transaction = Transaction(shareOne.name, 2, TransactionType.BUY, "20-11-2020")
 
-    val transactions = listOf(transaction)
-
-    init {
-        getHistory()
-    }
-
-    private fun getHistory() {
-        viewModelScope.launch {
-            try {
-
-            } catch (exception: Exception) {
+    // val transactions = listOf(transaction)
 
 
+    class Factory(
+        private val application: Application
+    ) : ViewModelProvider.Factory {
+
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(HistoryViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return HistoryViewModel(application) as T
             }
+            throw IllegalArgumentException("Unable to construct viewmodel")
         }
     }
 }
