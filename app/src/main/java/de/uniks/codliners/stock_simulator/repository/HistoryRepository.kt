7eb0700f9204d1fba3aebf1dc32f31454144c647
Juri <juri.lozowoj@freenet.de>
2020-02-18 09:49:45 +0100

    working on history repository

--- app/src/main/java/de/uniks/codliners/stock_simulator/repository/HistoryRepository.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/repository/HistoryRepository.kt
@@ -8,12 +8,22 @@ import de.uniks.codliners.stock_simulator.domain.Transaction
 
 class HistoryRepository(private val stockAppDatabase: StockAppDatabase) {
 
-    val transactions: LiveData<List<Transaction>> = Transformations.map(stockAppDatabase.transactionDao.getTransactions()) {
-        it?.transactionsAsDomainModel()
+    sealed class State {
+        object Loading: State()
+        object Done: State()
+        class Error(val message: String)
     }
+
+    val transactions: LiveData<List<Transaction>> =
+        Transformations.map(stockAppDatabase.transactionDao.getTransactions()) {
+            it?.transactionsAsDomainModel()
+        }
 
     fun transactionByShareName(shareName: String): LiveData<List<Transaction>> = Transformations
         .map(stockAppDatabase.transactionDao.getTransactionsByShareName(shareName)) {
             it?.transactionsAsDomainModel()
         }
+
+
+
 }
--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryViewModel.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryViewModel.kt
@@ -1,16 +1,36 @@
 package de.uniks.codliners.stock_simulator.ui.history
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import de.uniks.codliners.stock_simulator.domain.Share
 import de.uniks.codliners.stock_simulator.domain.Transaction
 import de.uniks.codliners.stock_simulator.domain.TransactionType
+import de.uniks.codliners.stock_simulator.repository.HistoryRepository
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class HistoryViewModel : ViewModel() {
+
+    private val historyRepository = HistoryRepository()
 
     val shareOne = Share("AA", "american airline", 20.0, 0.2, 0.2, 0.1)
     val shareTwo = Share("SMA", "sma ", 19.0, 0.2, 0.2, 0.1)
 
-    val transaction = Transaction(shareOne, 2,  TransactionType.BUY, "20-11-2020")
+    val transaction = Transaction(shareOne.name, 2,  TransactionType.BUY, "20-11-2020")
 
     val transactions = listOf(transaction)
+
+    init {
+        getHistory()
+    }
+
+    private fun getHistory() {
+        viewModelScope.launch {
+        try {
+
+        } catch (exception: Exception) {
+
+
+        }
+    }
 }
