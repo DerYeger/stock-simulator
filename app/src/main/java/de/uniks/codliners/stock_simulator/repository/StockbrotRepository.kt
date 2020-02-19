ef33f7a6fab2f3632097c7167779b03538fdd930
lucasheld <lucasheld@hotmail.de>
2020-02-19 09:52:34 +0100

    implemented enable disable stockbrot in repository

--- app/src/main/java/de/uniks/codliners/stock_simulator/repository/StockbrotRepository.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/repository/StockbrotRepository.kt
@@ -3,6 +3,9 @@ package de.uniks.codliners.stock_simulator.repository
 import android.content.Context
 import de.uniks.codliners.stock_simulator.database.StockAppDatabase
 import de.uniks.codliners.stock_simulator.database.getDatabase
+import de.uniks.codliners.stock_simulator.domain.Quote
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class StockbrotRepository(private val database: StockAppDatabase) {
 
@@ -10,6 +13,22 @@ class StockbrotRepository(private val database: StockAppDatabase) {
 
     val quotes by lazy {
         database.stockbrotDao.getQuotes()
+    }
+
+    suspend fun enableStockbrotControl(quote: Quote) {
+        withContext(Dispatchers.IO) {
+            database.stockbrotDao.apply {
+                insertQuote(quote)
+            }
+        }
+    }
+
+    suspend fun disableStockbrotControl(quote: Quote) {
+        withContext(Dispatchers.IO) {
+            database.stockbrotDao.apply {
+                deleteQuoteBySymbol(quote.symbol)
+            }
+        }
     }
 
 }
