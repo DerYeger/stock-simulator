0230c5bbcd9e546a482abc747b46066eb609ec5d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-02-19 15:10:02 +0100

    Real Balance Data For Diagram

--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/account/AccountViewModel.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/account/AccountViewModel.kt
@@ -10,7 +10,7 @@ class AccountViewModel(application: Application) : ViewModel() {
     private val accountRepository = AccountRepository(application)
 
     val balance = accountRepository.latestBalance
-
+    val balancesLimited = accountRepository.balancesLimited
     val depotQuotes = accountRepository.depot
 
     class Factory(
