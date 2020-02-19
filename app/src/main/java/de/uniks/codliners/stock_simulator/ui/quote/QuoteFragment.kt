842c880ad1952e4b7413ec17e6f5369aaeaac867
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-02-19 16:11:11 +0100

    Refactor Charts To Utils

--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/quote/QuoteFragment.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/quote/QuoteFragment.kt
@@ -1,7 +1,5 @@
 package de.uniks.codliners.stock_simulator.ui.quote
 
-import android.graphics.Color
-import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,15 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
-import com.github.mikephil.charting.utils.ColorTemplate
-import de.uniks.codliners.stock_simulator.database.HistoricalPrice
 import de.uniks.codliners.stock_simulator.databinding.FragmentQuoteBinding
+import de.uniks.codliners.stock_simulator.initLineChart
+import de.uniks.codliners.stock_simulator.updateLineChart
 import java.text.SimpleDateFormat
 
 class QuoteFragment : Fragment() {
@@ -31,22 +24,14 @@ class QuoteFragment : Fragment() {
 
     private lateinit var binding: FragmentQuoteBinding
 
-    private lateinit var tfLight: Typeface
-    private lateinit var tfRegular: Typeface
-    private lateinit var chart: LineChart
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        tfLight = Typeface.createFromAsset(context!!.assets, "OpenSans-Light.ttf")
-        tfRegular = Typeface.createFromAsset(context!!.assets, "OpenSans-Regular.ttf")
-
         binding = FragmentQuoteBinding.inflate(inflater)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-        chart = binding.quoteChart
 
         viewModel.errorAction.observe(viewLifecycleOwner, Observer { errorMessage: String? ->
             errorMessage?.let {
@@ -56,15 +41,18 @@ class QuoteFragment : Fragment() {
         })
 
         viewModel.historicalPrices.observe(viewLifecycleOwner, Observer { priceList ->
-            priceList?.let {
-                drawGraph(priceList)
+            run {
+                val simpleDateFormat =
+                    SimpleDateFormat("yyyy-MM-dd", resources.configuration.locale)
+                val entries = priceList.map { price ->
+                    val timestamp = simpleDateFormat.parse(price.date)!!.time
+                    Entry(timestamp.toFloat(), price.close.toFloat())
+                }
+                updateLineChart(binding.quoteChart, entries, "Historical Prices")
             }
         })
 
-        if (viewModel.historicalPrices.value != null) {
-            drawGraph(viewModel.historicalPrices.value!!)
-        }
-
+        initLineChart(binding.quoteChart, context!!, resources.configuration.locale)
 
         return binding.root
     }
@@ -72,63 +60,5 @@ class QuoteFragment : Fragment() {
     private fun showErrorToast(errorMessage: String?) {
         Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show()
         findNavController().navigateUp()
-    }
-
-    private fun drawGraph(prices: List<HistoricalPrice>) {
-        chart.data = generateLineData(prices)
-        styleGraph(chart)
-
-        chart.invalidate()
-    }
-
-    private fun styleGraph(chart: LineChart) {
-        val xAxis = chart.xAxis
-        val yAxis = chart.axisLeft
-
-        chart.axisRight.isEnabled = false
-        chart.description.isEnabled = false
-        chart.legend.isEnabled = false
-        chart.setBackgroundColor(Color.TRANSPARENT)
-        chart.setDrawGridBackground(false)
-        chart.setTouchEnabled(false)
-
-        xAxis.axisLineColor = Color.TRANSPARENT
-        xAxis.gridColor = Color.DKGRAY
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.textColor = Color.WHITE
-        xAxis.typeface = tfLight
-
-        yAxis.axisLineColor = Color.TRANSPARENT
-        yAxis.gridColor = Color.DKGRAY
-        yAxis.setLabelCount(2, false)
-        yAxis.textColor = Color.WHITE
-        yAxis.typeface = tfLight
-    }
-
-    private fun generateLineData(prices: List<HistoricalPrice>): LineData {
-
-        val lineDataValues = prices.map { price ->
-            val date = SimpleDateFormat("yyyy-MM-dd").parse(price.date)
-            Entry(date.time.toFloat(), price.close.toFloat())
-        }
-
-        val lds = LineDataSet(lineDataValues, "Account Balance")
-        lds.color = Color.WHITE
-        lds.color = ColorTemplate.VORDIPLOM_COLORS[0]
-        lds.cubicIntensity = 0.2f
-        lds.fillAlpha = 100
-        lds.fillColor = Color.WHITE
-        lds.lineWidth = 1.8f
-        lds.mode = LineDataSet.Mode.CUBIC_BEZIER
-        lds.setDrawCircles(false)
-        lds.setDrawFilled(true)
-        lds.setDrawHorizontalHighlightIndicator(false)
-        val sets: ArrayList<ILineDataSet> = ArrayList()
-        sets.add(lds)
-
-        val ld = LineData(sets)
-        ld.setDrawValues(false)
-
-        return ld
     }
 }
