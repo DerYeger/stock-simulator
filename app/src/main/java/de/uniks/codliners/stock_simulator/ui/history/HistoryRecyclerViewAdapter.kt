437985386d08c13cc740c2f5c838803acb111ff3
Jan Müller <janmueller3698@gmail.com>
2020-02-19 11:20:48 +0100

    Changed transaction view to card design

--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryRecyclerViewAdapter.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryRecyclerViewAdapter.kt
@@ -5,13 +5,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import de.uniks.codliners.stock_simulator.databinding.TransactionViewBinding
+import de.uniks.codliners.stock_simulator.databinding.TransactionCardBinding
 import de.uniks.codliners.stock_simulator.domain.Transaction
 
 class HistoryRecyclerViewAdapter: ListAdapter<Transaction,
         HistoryRecyclerViewAdapter.ViewHolder>(DiffCallback) {
 
-    inner class ViewHolder(private val binding: TransactionViewBinding) :
+    inner class ViewHolder(private val binding: TransactionCardBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(transaction: Transaction) {
@@ -22,7 +22,7 @@ class HistoryRecyclerViewAdapter: ListAdapter<Transaction,
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
-            TransactionViewBinding.inflate(
+            TransactionCardBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false)
         )
     }
