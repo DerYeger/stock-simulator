e659b4c23e4253c687028e4d5e356b09e7c327e2
Jan Müller <janmueller3698@gmail.com>
2020-02-24 17:52:56 +0100

    Added ProgressBar to SearchFragment, indicating a search in progress
    This closes #34

--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/search/SearchViewModel.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/search/SearchViewModel.kt
@@ -1,7 +1,9 @@
 package de.uniks.codliners.stock_simulator.ui.search
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import de.uniks.codliners.stock_simulator.domain.Symbol
 import de.uniks.codliners.stock_simulator.repository.SymbolRepository
 import de.uniks.codliners.stock_simulator.sourcedLiveData
@@ -14,12 +16,35 @@ class SearchViewModel(application: Application) : ViewModel() {
 
     val searchQuery = MutableLiveData<String>()
     val typeFilter = MutableLiveData<String>()
+    private val searching = MutableLiveData<Boolean>(false)
 
+    // The search implementation should be refactored, as it currently runs on the UI-Thread
     val searchResults = sourcedLiveData(symbols, searchQuery, typeFilter) {
+        // Manually set isLoading to true, as the UI-Thread is blocked by the search. This should be refactored in the future
+        (isLoading as MutableLiveData<Boolean>).value = true
+
+        searching.value = true
         symbols.value?.filtered(
             query = searchQuery.value,
             typeFilter = typeFilter.value
-        )
+        ).also {
+            searching.value = false
+
+            // Manually set isLoading to false, as the UI-Thread is blocked by the search. This should be refactored in the future
+            isLoading.value = false
+        }
+    }
+
+    val isLoading = sourcedLiveData(symbols, searching) {
+        symbols.value?.size ?: 0 == 0 || searching.value ?: false
+    }
+
+    val hasResults = sourcedLiveData(searchResults, isLoading) {
+        searchResults.value?.size ?: 0 > 0 && !(isLoading.value ?: false)
+    }
+
+    val hasNoResults = sourcedLiveData(searchResults, isLoading) {
+        searchResults.value?.size == 0 && !(isLoading.value ?: false)
     }
 
     private fun List<Symbol>.filtered(query: String?, typeFilter: String?): List<Symbol> {
