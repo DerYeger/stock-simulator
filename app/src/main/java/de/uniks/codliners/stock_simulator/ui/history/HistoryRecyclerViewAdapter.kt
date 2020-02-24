090d6a898559bb978940b3828f7e8a318661b77d
Juri <juri.lozowoj@freenet.de>
2020-02-24 14:47:30 +0100

    fixed time display in history card

--- app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryRecyclerViewAdapter.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/ui/history/HistoryRecyclerViewAdapter.kt
@@ -17,7 +17,7 @@ class HistoryRecyclerViewAdapter(
 ) : ListAdapter<Transaction,
         HistoryRecyclerViewAdapter.ViewHolder>(DiffCallback) {
 
-    private val dateFormatter = SimpleDateFormat("dd.MM.yyyy hh:mm", locale)
+    private val dateFormatter = SimpleDateFormat("dd.MM.yyyy HH:mm", locale)
 
     inner class ViewHolder(private val binding: TransactionCardBinding) :
         RecyclerView.ViewHolder(binding.root) {
