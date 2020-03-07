d1199155d06baff0ae50b80491db2fa372371a0a
Jan Müller <janmueller3698@gmail.com>
2020-03-07 15:52:11 +0100

    Documented QuoteRepository

--- app/src/main/java/de/uniks/codliners/stock_simulator/repository/QuoteRepository.kt
+++ app/src/main/java/de/uniks/codliners/stock_simulator/repository/QuoteRepository.kt
@@ -11,16 +11,19 @@ import de.uniks.codliners.stock_simulator.network.NetworkService
 import de.uniks.codliners.stock_simulator.network.asDomainHistoricalPrices
 import de.uniks.codliners.stock_simulator.network.asDomainQuote
 import de.uniks.codliners.stock_simulator.network.asHistoricalPrices
+import de.uniks.codliners.stock_simulator.repository.QuoteRepository.State
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 /**
- * TODO
+ * Repository for accessing and updating [Quote]s.
  *
- * @property database TODO
+ * @property database The database used by this repository.
+ * @property state The current [State] of this repository.
  *
- * @author TODO
+ * @author Jan Müller
  * @author Jonas Thelemann
+ * @author Juri Lozowoj
  */
 class QuoteRepository(private val database: StockAppDatabase) {
 
@@ -41,55 +44,80 @@ class QuoteRepository(private val database: StockAppDatabase) {
     }
     val state: LiveData<State> = _state
 
-    fun quoteWithId(symbol: String): LiveData<Quote> =
-        database.quoteDao.getQuoteWithId(symbol)
+    /**
+     * Returns a [LiveData](https://developer.android.com/reference/android/arch/lifecycle/LiveData) containing the [Quote] with the requested id or null if no such [Quote] exists.
+     *
+     * @param id The id of the requested [Quote].
+     * @return [LiveData](https://developer.android.com/reference/android/arch/lifecycle/LiveData) containing the [Quote] with the requested id or null if no such [Quote] exists.
+     */
+    fun quoteWithId(id: String): LiveData<Quote> =
+        database.quoteDao.getQuoteWithId(id)
 
-    fun quoteById(symbol: String): Quote? =
-        database.quoteDao.getQuoteValueById(symbol)
+    /**
+     * Returns the [Quote] with the requested id or null if no such [Quote] exists.
+     *
+     * @param id The id of the requested [Quote].
+     * @return [Quote] with the requested id or null if no such [Quote] exists.
+     */
+    fun quoteById(id: String): Quote? =
+        database.quoteDao.getQuoteValueById(id)
 
-    fun historicalPrices(symbol: String): LiveData<List<HistoricalPrice>> =
-        database.historicalDao.getHistoricalPricesById(symbol)
 
-    suspend fun fetchIEXQuote(symbol: String): Boolean {
-        var conncetionSucces: Boolean = false
-        withContext(Dispatchers.IO) {
-            try {
-                _state.postValue(State.Refreshing)
-                val quote = NetworkService.IEX_API.quote(symbol).asDomainQuote()
-                database.quoteDao.insert(quote)
-                val historicalPrices = NetworkService.IEX_API
-                    .historicalPrices(symbol = symbol, chartCloseOnly = true)
-                    .asDomainHistoricalPrices(symbol)
-                database.historicalDao.deleteHistoricalPricesById(symbol)
-                database.historicalDao.insertAll(*historicalPrices.toTypedArray())
-                _state.postValue(State.Done)
-                conncetionSucces = true
-            } catch (exception: Exception) {
-                _state.postValue(State.Error(exception.message ?: "Oops!"))
-            }
+    /**
+     * Returns a [LiveData](https://developer.android.com/reference/android/arch/lifecycle/LiveData) containing a [List] of [HistoricalPrice]s for the requested id or null if no such [HistoricalPrice]s exists.
+     *
+     * @param id The id of the requested [HistoricalPrice]s.
+     * @return [LiveData](https://developer.android.com/reference/android/arch/lifecycle/LiveData) containing a [List] of [HistoricalPrice]s for the requested id or null if no such [HistoricalPrice]s exists.
+     */
+    fun historicalPrices(id: String): LiveData<List<HistoricalPrice>> =
+        database.historicalDao.getHistoricalPricesById(id)
+
+    /**
+     * Fetches the [Quote] and [HistoricalPrice]s for the requested symbol from the IEX API, then stores them in the [StockAppDatabase].
+     *
+     * @param symbol The IEX symbol of the share.
+     * @return true if the operation was successful and false otherwise.
+     */
+    suspend fun fetchIEXQuote(symbol: String): Boolean = withContext(Dispatchers.IO) {
+        try {
+            _state.postValue(State.Refreshing)
+            val quote = NetworkService.IEX_API.quote(symbol).asDomainQuote()
+            database.quoteDao.insert(quote)
+            val historicalPrices = NetworkService.IEX_API
+                .historicalPrices(symbol = symbol, chartCloseOnly = true)
+                .asDomainHistoricalPrices(symbol)
+            database.historicalDao.deleteHistoricalPricesById(symbol)
+            database.historicalDao.insertAll(*historicalPrices.toTypedArray())
+            _state.postValue(State.Done)
+            true
+        } catch (exception: Exception) {
+            _state.postValue(State.Error(exception.message ?: "Oops!"))
+            false
         }
-        return conncetionSucces
     }
 
-    suspend fun fetchCoinGeckoQuote(id: String): Boolean {
-        var conncetionSucces: Boolean = false
-        withContext(Dispatchers.IO) {
-            try {
-                _state.postValue(State.Refreshing)
-                val quote = NetworkService.COINGECKO_API.quote(id).asDomainQuote()
-                database.quoteDao.insert(quote)
-                val historicalPrices = NetworkService.COINGECKO_API
-                    .historicalPrices(id = id)
-                    .asHistoricalPrices(id)
-                database.historicalDao.deleteHistoricalPricesById(id)
-                database.historicalDao.insertAll(*historicalPrices.toTypedArray())
-                _state.postValue(State.Done)
-                conncetionSucces = true
-            } catch (exception: Exception) {
-                _state.postValue(State.Error(exception.message ?: "Oops!"))
-            }
+    /**
+     * Fetches the [Quote] and [HistoricalPrice]s for the requested symbol from the CoinGecko API, then stores them in the [StockAppDatabase].
+     *
+     * @param id The CoinGecko id of the cryptocurrency.
+     * @return true if the operation was successful and false otherwise.
+     */
+    suspend fun fetchCoinGeckoQuote(id: String): Boolean = withContext(Dispatchers.IO) {
+        try {
+            _state.postValue(State.Refreshing)
+            val quote = NetworkService.COINGECKO_API.quote(id).asDomainQuote()
+            database.quoteDao.insert(quote)
+            val historicalPrices = NetworkService.COINGECKO_API
+                .historicalPrices(id = id)
+                .asHistoricalPrices(id)
+            database.historicalDao.deleteHistoricalPricesById(id)
+            database.historicalDao.insertAll(*historicalPrices.toTypedArray())
+            _state.postValue(State.Done)
+            true
+        } catch (exception: Exception) {
+            _state.postValue(State.Error(exception.message ?: "Oops!"))
+            false
         }
-        return conncetionSucces
     }
 
     /**
@@ -99,9 +127,7 @@ class QuoteRepository(private val database: StockAppDatabase) {
      */
     suspend fun resetQuotes() {
         withContext(Dispatchers.IO) {
-            database.quoteDao.apply {
-                deleteQuotes()
-            }
+            database.quoteDao.deleteQuotes()
         }
     }
 }
